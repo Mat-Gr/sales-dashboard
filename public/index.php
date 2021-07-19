8fae26b2b35644f55965903bdf64c08e05927e5b
Mateusz <mateusz.grochala@yahoo.dk>
2021-07-19 20:01:52 +0200

    🏗️ Register route & controller pairs in index file

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 
 require dirname(__DIR__, 1) . '/vendor/autoload.php';
+
+use App\Controllers\DashboardController;
+use App\Lib\Router;
+
+echo Router::getResponse([
+    Router::get('/', new DashboardController, 'viewDashboard'),
+]);
