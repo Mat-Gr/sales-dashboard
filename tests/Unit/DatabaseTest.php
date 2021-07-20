cf26ededa4c4aecdb07c4bd25e5416c4015b19ad
Mateusz <mateusz.grochala@yahoo.dk>
2021-07-20 23:23:03 +0200

    ✅ Make the select test more strict and defined

--- tests/Unit/DatabaseTest.php
+++ tests/Unit/DatabaseTest.php
@@ -20,10 +20,24 @@ class DatabaseTest extends TestCase
 
     public function testCanSelectFromCustomersTable()
     {
+        DatabaseConnection::connection()->exec('DELETE FROM '.Customer::TABLE);
+        DatabaseConnection::connection()
+            ->exec("INSERT INTO ".Customer::TABLE." (first_name, last_name, email) values ('Jane', 'Doe', 'jane.doe@example.com')");
+        $id = DatabaseConnection::connection()->lastInsertId();
+
         $rows = Customer::select(['first_name', 'last_name', 'email']);
 
         $this->assertIsArray($rows);
-        $this->assertInstanceOf(Customer::class, $rows[0]);
-    }
+        $this->assertNotEmpty($rows);
 
+        $customer = $rows[0];
+
+        $this->assertInstanceOf(Customer::class, $customer);
+        $this->assertEquals($customer->first_name, 'Jane');
+        $this->assertEquals($customer->last_name, 'Doe');
+        $this->assertEquals($customer->email, 'jane.doe@example.com');
+        $this->assertEquals($customer->id, $id);
+
+        DatabaseConnection::connection()->exec('DELETE FROM '.Customer::TABLE);
+    }
 }
