4a0b2d6f5409b513f80ae91f8c83a3f4e7778eb1
Mateusz <mateusz.grochala@yahoo.dk>
2021-07-21 20:51:42 +0200

    ✨ Basic index endpoint for fetching a list of Orders

--- public/index.php
+++ public/index.php
@@ -3,9 +3,11 @@
 require dirname(__DIR__, 1) . '/vendor/autoload.php';
 
 use App\Controllers\DashboardController;
+use App\Controllers\OrdersController;
 use App\Lib\Router;
 
 echo Router::getResponse([
     Router::get('/', new DashboardController, 'viewDashboard'),
     Router::get('/api/seed', new DashboardController, 'seedDatabase'),
+    Router::get('/api/orders', new OrdersController, 'index'),
 ]);
