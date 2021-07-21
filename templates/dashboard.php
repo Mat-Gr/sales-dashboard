4b59318c09f7e6a7842b7412e6f3d36e1f2aa262
Mateusz <mateusz.grochala@yahoo.dk>
2021-07-21 21:11:46 +0200

    💄 Add basic Bootstrap support

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -4,11 +4,16 @@
     <meta charset="utf-8"/>
     <meta http-equiv="x-ua-compatible" content="ie=edge, chrome=1"/>
     <title>Dashboard</title>
-    <link rel="stylesheet" href=""/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossorigin="anonymous">
 </head>
-<body>
-    <section>
+<body class="py-4">
+<main>
+    <div class="container">
         <h1>Welcome to the Dashboard</h1>
-    </section>
+    </div>
+</main>
 </body>
 </html>
