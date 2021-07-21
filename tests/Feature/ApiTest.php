4ee0fc8d6ca1ee5a7cb88d11b092d9cca03eab7e
Mateusz <mateusz.grochala@yahoo.dk>
2021-07-21 22:57:36 +0200

    🧪 Test grouped Order result

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -4,6 +4,10 @@
 namespace Tests\Feature;
 
 
+use App\Lib\DatabaseConnection;
+use App\Models\Customer;
+use App\Models\Order;
+use App\Models\OrderItem;
 use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
@@ -25,5 +29,30 @@ class ApiTest extends TestCase
 
         $this->assertArrayHasKey('total', $body);
         $this->assertIsNumeric($body['total']);
+    }
+
+    public function testCanGetGroupedOrdersCount()
+    {
+        $client = new Client(['base_uri' => 'http://localhost']);
+        $client->request('GET', '/api/seed');
+
+        $client   = new Client(['base_uri' => 'http://localhost']);
+        $response = $client->request('GET', '/api/orders/grouped');
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = $response->getBody()->getContents();
+
+        $this->assertJson($body);
+
+        $body = json_decode($body, true, 512, JSON_THROW_ON_ERROR);
+
+        $this->assertArrayHasKey('collection', $body);
+        $this->assertArrayHasKey('purchase_date', $body['collection'][0]);
+        $this->assertArrayHasKey('total', $body['collection'][0]);
+
+        DatabaseConnection::connection()->exec('DELETE FROM '.OrderItem::TABLE);
+        DatabaseConnection::connection()->exec('DELETE FROM '.Order::TABLE);
+        DatabaseConnection::connection()->exec('DELETE FROM '.Customer::TABLE);
     }
 }
