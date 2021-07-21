1e234e1507c3e9fc38addf19e56ad1ac05f18a68
Mateusz <mateusz.grochala@yahoo.dk>
2021-07-21 17:12:38 +0200

    ♻️ Move Model related logic to the abstract class, and made it more reusable

--- src/Models/Abstracts/Model.php
+++ src/Models/Abstracts/Model.php
@@ -3,7 +3,32 @@
 namespace App\Models\Abstracts;
 
 
+use App\Lib\DatabaseConnection;
+use PDO;
+use PDOStatement;
+
 abstract class Model
 {
+    public static array $fillable = [];
 
+    public static function select(): array
+    {
+        $query = DatabaseConnection::connection()->query('SELECT * from ' . static::TABLE);
+        $query->setFetchMode(PDO::FETCH_CLASS, static::class);
+
+        return $query->fetchAll();
+    }
+
+    public function insert(): PDOStatement|false
+    {
+        $columns = implode(', ', static::$fillable);
+        $bindings = implode(', ', array_map(fn (string $binding) => ":$binding", static::$fillable));
+
+        $query = DatabaseConnection::connection()
+            ->prepare("INSERT INTO ".static::TABLE." ($columns) value ($bindings)");
+
+        $result = $query->execute((array)$this);
+
+        return $result ? $query : false;
+    }
 }
--- src/Models/Customer.php
+++ src/Models/Customer.php
@@ -3,10 +3,7 @@
 namespace App\Models;
 
 
-use App\Lib\DatabaseConnection;
 use App\Models\Abstracts\Model;
-use PDO;
-use PDOStatement;
 
 class Customer extends Model
 {
@@ -16,24 +13,10 @@ class Customer extends Model
     public string $last_name;
     public string $email;
 
-	public static function select(array $columns = []): array
-	{
-		if (empty($columns)) {
-			$columns = ['*'];
-		}
+    public static array $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+    ];
 
-		$query = DatabaseConnection::connection()->query('SELECT * from ' . self::TABLE);
-		$query->setFetchMode(PDO::FETCH_CLASS, self::class);
-
-		return $query->fetchAll();
-	}
-
-    public function insert(): PDOStatement|false
-    {
-        $query = DatabaseConnection::connection()->prepare("INSERT INTO " . self::TABLE . " (first_name, last_name, email) value (:first_name, :last_name, :email)");
-
-        $result = $query->execute((array)$this);
-
-        return $result ? $query : false;
-	}
 }
